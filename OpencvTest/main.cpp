1f2980ef3984349a13c91e1a09f5e4de119df675
hmuronaka <capriccio27@gmail.com>
2015-01-14 15:23:08 +0900

    Initial Commit

---
+++ OpencvTest/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  OpencvTest
+//
+//  Created by Muronaka Hiroaki on 2015/01/14.
+//  Copyright (c) 2015年 Muronaka Hiroaki. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
